662dfe5d97e31ba02000532ad2540a072f479a62
Matt <mattg317@gmail.com>
2017-01-08 14:09:17 -0500

    switch to NYT

---
+++ app/app.js
@@ -0,0 +1,8 @@
+var React = require("react");
+var ReactDOM = require("react-DOM");
+
+ReactDOM.render(<div className="jumbotron">
+  <h1>New York Times Article Scrubber</h1>
+  <p>Search for an annotate articles of interest!</p>
+ </div>, 
+document.getElementById('app'))
